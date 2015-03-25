b98ee71da39b32b2542c1f1c16edbe7dacef559c
Rumen Stankov <rumen.stankov@gmail.com>
2015-03-25 13:18:37 +0200

    Fixed a bug in Series serialization for series count > 2. Updated Examples.

--- MVC_Demo/Controllers/DemoController.cs
+++ MVC_Demo/Controllers/DemoController.cs
@@ -16,9 +16,10 @@ namespace MVC_Demo.Controllers
         {
             List<double> tokyoValues = new List<double> { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 };
             List<double> nyValues = new List<double> { -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5 };
-            List<double> vikValues = new List<double> { -20, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5 };
+            List<double> vikValues = new List<double> { -20, 11.8, 15.7, 21.3, 7.0, 21.0, 34.8, 25.1, 21.1, 15.1, 9.6, 1.5 };
             List<SeriesData> tokyoData = new List<SeriesData>();
             List<SeriesData> nyData = new List<SeriesData>();
+            List<SeriesData> vikData = new List<SeriesData>();
 
             foreach (double value in tokyoValues)
             {
@@ -28,10 +29,14 @@ namespace MVC_Demo.Controllers
             {
                 nyData.Add(new SeriesData { Y = value });
             }
+            foreach (double value in vikValues)
+            {
+                vikData.Add(new SeriesData { Y = value });
+            }
 
             ViewData["tokyoData"] = tokyoData;
             ViewData["nyData"] = nyData;
-            ViewData["vikData"] = nyData;
+            ViewData["vikData"] = vikData;
 
             return View();
         }      
