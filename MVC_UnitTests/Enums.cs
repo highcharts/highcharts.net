61194bd01ebe91e373ffaea50747019bd9894207
Rumen Stankov <rumen.stankov@gmail.com>
2015-04-01 15:35:21 +0300

    Add tests for Strings and Enums

---
+++ MVC_UnitTests/Enums.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Highsoft.Web.Mvc;
+using Highsoft.Web.Mvc.Rendering;
+
+namespace Highsoft.Web.Mvc.UnitTests
+{
+    [TestClass]
+    public class Chart
+    {
+        Highcharts _chart;
+        HighchartsRenderer _renderer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _chart = new Highcharts();
+            _renderer = new HighchartsRenderer(_chart);
+        }
+
+        [TestMethod]
+        public void TestAreaType()
+        {
+            _chart.Chart.SpacingLeft = 10;
+            _chart.Chart.Type = ChartType.Area;
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"type\":\"area\""));            
+        }
+
+        [TestMethod]
+        public void TestPieType()
+        {
+            _chart.Chart.Type = ChartType.Pie;
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"type\":\"pie\""));
+        }
+
+        [TestMethod]
+        public void TestDefaultType() // should be line
+        {
+            string json = _renderer.RenderHtml();
+
+            Assert.IsFalse(json.Contains("\"type\":\"line\""));
+        }
+
+        [TestMethod]
+        public void TestEnumMappings() // should be line
+        {
+            _chart.Exporting.Type = ExportingType.Applicationpdf;
+            string json = _renderer.RenderHtml();
+
+            Assert.IsFalse(json.Contains("\"type\":\"application/pdf\""));
+        }        
+    }
+}
---
+++ MVC_UnitTests/Strings.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Highsoft.Web.Mvc;
+using Highsoft.Web.Mvc.Rendering;
+
+namespace Highsoft.Web.Mvc.UnitTests
+{
+    [TestClass]
+    public class Strings
+    {
+        Highcharts _chart;
+        HighchartsRenderer _renderer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _chart = new Highcharts();
+            _renderer = new HighchartsRenderer(_chart);
+        }
+
+        [TestMethod]
+        public void TestForNull()
+        {
+            _chart.Subtitle.Text = null;
+            string json = _renderer.RenderHtml();
+
+            Assert.IsFalse(json.Contains("\"text\":\"null\""));
+            Assert.IsFalse(json.Contains("\"text\":null"));
+            Assert.IsFalse(json.Contains("\"text\""));
+        }
+
+        [TestMethod]
+        public void TestFrench()
+        {
+            _chart.Legend.LabelFormat = "Départementales à Mayotte : un électeur fraudeur pris la main dans le sac";
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"labelFormat\":\"Départementales à Mayotte : un électeur fraudeur pris la main dans le sac\""));
+        }
+
+        [TestMethod]
+        public void TestNorwegian()
+        {
+            _chart.Legend.LabelFormat = "Verksemda er ein del av næringsklynga ";
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"labelFormat\":\"Verksemda er ein del av næringsklynga \""));
+        }
+
+        [TestMethod]
+        public void TestJapanese()
+        {
+            _chart.Legend.LabelFormat = "ブラック企業 国が特別対策班";
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"labelFormat\":\"ブラック企業 国が特別対策班\""));
+        }
+
+        [TestMethod]
+        public void TestLegendLabelFormat()
+        {
+            _chart.Legend.LabelFormat = "%d";
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"labelFormat\":\"%d\""));
+        }
+
+        [TestMethod]
+        public void TestTitleText()
+        {
+            _chart.Title.Text = "Temperature";
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"text\":\"Temperature\""));
+        }
+
+        [TestMethod]
+        public void TestPlotOptionsAreaColor()
+        {
+            _chart.PlotOptions.Area.Color = "#FFAADD";
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"color\":\"#FFAADD\""));
+        }
+    }
+}
