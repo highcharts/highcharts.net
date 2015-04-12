7335485bfdd2acd30fda2fa53f34ef90950150ea
Rumen Stankov <rumen.stankov@gmail.com>
2015-04-13 02:23:14 +0300

    Updates to the custom serialization of the Animation property + 5 new Unit Tests testing it

---
+++ MVC_UnitTests/Animation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Highsoft.Web.Mvc;
+using Highsoft.Web.Mvc.Rendering;
+
+namespace Highsoft.Web.Mvc.UnitTests
+{
+    [TestClass]
+    public class Animation
+    {
+        Highcharts _chart;
+        HighchartsRenderer _renderer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _chart = new Highcharts();
+            _renderer = new HighchartsRenderer(_chart);
+        }
+
+        [TestMethod]
+        public void TestDefault()
+        {            
+            string json = _renderer.RenderHtml();
+
+            Assert.IsFalse(json.Contains("animation"));
+        }
+
+        [TestMethod]
+        public void TestBooleanFalse()
+        {
+            _chart.Chart.Animation.Enabled = false;
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"animation\":false"));            
+        }
+
+        [TestMethod]
+        public void TestDuration()
+        {
+            _chart.Chart.Animation.Duration = 775;
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"duration\":775"));
+        }
+
+        [TestMethod]
+        public void TestEasing()
+        {
+            _chart.Chart.Animation.Easing = "cube";
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"easing\":\"cube\""));
+        }
+
+        
+    }
+}
