5b7909a825e445f8dc8aa1a95fb32157b07bfdf5
Rumen Stankov <rumen.stankov@gmail.com>
2015-06-20 12:02:12 +0300

    Add Show Controller  Code and Show View Code buttons to all examples, so that customers can be the actual code

--- MVC_Demo/Areas/Highcharts/Controllers/Demo/DemoController.cs
+++ MVC_Demo/Areas/Highcharts/Controllers/Demo/DemoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Highsoft.Web.Mvc;
+using System.IO;
 
 namespace MVC_Demo.Areas.Highcharts.Controllers.Demo
 {
@@ -20,6 +21,13 @@ namespace MVC_Demo.Areas.Highcharts.Controllers.Demo
         public ActionResult Gallery(string demo)
         {
             ViewBag.Demo = demo;
+
+            string text = System.IO.File.ReadAllText(Server.MapPath("~/Areas/Highcharts/Controllers/Shared/" + demo + "Controller.cs"));
+            ViewBag.ControllerCode = text;
+
+            text = System.IO.File.ReadAllText(Server.MapPath("~/Areas/Highcharts/Views/Shared/" + demo + ".cshtml"));
+            ViewBag.ViewCode = text;
+
             return View();
         }
 
@@ -27,104 +35,6 @@ namespace MVC_Demo.Areas.Highcharts.Controllers.Demo
         {
             return RedirectToAction("Index", new { id = "dark-unica" });
         }
-
-        public ActionResult LineBasic()
-        {
-            List<double> tokyoValues = new List<double> { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 };
-            List<double> nyValues = new List<double> { -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5 };
-            List<double> berlinValues = new List<double> { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 };
-            List<double> londonValues = new List<double> { 3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8 };
-            List<LineSeriesData> tokyoData = new List<LineSeriesData>();
-            List<LineSeriesData> nyData = new List<LineSeriesData>();
-            List<LineSeriesData> berlinData = new List<LineSeriesData>();
-            List<LineSeriesData> londonData = new List<LineSeriesData>();
-
-            foreach (double value in tokyoValues)
-            {
-                tokyoData.Add(new LineSeriesData { Y = value });
-            }
-            foreach (double value in nyValues)
-            {
-                nyData.Add(new LineSeriesData { Y = value });
-            }
-            foreach (double value in berlinValues)
-            {
-                berlinData.Add(new LineSeriesData { Y = value });
-            }
-            foreach (double value in londonValues)
-            {
-                londonData.Add(new LineSeriesData { Y = value });
-            }
-
-
-            ViewData["tokyoData"] = tokyoData;
-            ViewData["nyData"] = nyData;
-            ViewData["berlinData"] = berlinData;
-            ViewData["londonData"] = londonData;
-
-            return View();
-        }
-
-        public ActionResult LineLabels()
-        {
-            List<double> tokyoValues = new List<double> { 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6 };
-            List<double> nyValues = new List<double> { -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5 };
-            List<double> berlinValues = new List<double> { -0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0 };
-            List<double> londonValues = new List<double> { 3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8 };
-            List<LineSeriesData> tokyoData = new List<LineSeriesData>();
-            List<LineSeriesData> nyData = new List<LineSeriesData>();
-            List<LineSeriesData> berlinData = new List<LineSeriesData>();
-            List<LineSeriesData> londonData = new List<LineSeriesData>();
-
-            foreach (double value in tokyoValues)
-            {
-                tokyoData.Add(new LineSeriesData { Y = value });
-            }
-            foreach (double value in nyValues)
-            {
-                nyData.Add(new LineSeriesData { Y = value });
-            }
-            foreach (double value in berlinValues)
-            {
-                berlinData.Add(new LineSeriesData { Y = value });
-            }
-            foreach (double value in londonValues)
-            {
-                londonData.Add(new LineSeriesData { Y = value });
-            }
-
-
-            ViewData["tokyoData"] = tokyoData;
-            ViewData["nyData"] = nyData;
-            ViewData["berlinData"] = berlinData;
-            ViewData["londonData"] = londonData;
-
-            return View();
-        }
-
-        public ActionResult LineLog()
-        {
-            List<double> logValues = new List<double> { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
-            List<LineSeriesData> logData = new List<LineSeriesData>();
-
-            logValues.ForEach(p => logData.Add(new LineSeriesData { Y = p }));
-
-            ViewData["logData"] = logData;
-
-            return View();
-        }
-
-        public ActionResult SplineInverted()
-        {
-            List<double> logValues = new List<double> { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
-            List<LineSeriesData> logData = new List<LineSeriesData>();
-
-            logValues.ForEach(p => logData.Add(new LineSeriesData { Y = p }));
-
-            ViewData["logData"] = logData;
-
-            return View();
-        } 
 
     }
 }
