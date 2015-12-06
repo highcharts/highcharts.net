b4637277e4887ea2c6c42dbcc9e4a25ca4c21cf1
Rumen Stankov <rumen.stankov@gmail.com>
2015-12-07 00:31:04 +0200

    Commit the Xaxis unit tests class

---
+++ MVC_UnitTests/XAxis.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Highsoft.Web.Mvc.Charts;
+using Highsoft.Web.Mvc.Charts.Rendering;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+
+namespace Highsoft.Web.Mvc.UnitTests
+{
+    [TestClass]
+    public class Xaxis
+    {
+        Highsoft.Web.Mvc.Charts.Highcharts _chart;
+        HighchartsRenderer _renderer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _chart = new Highcharts();
+            _renderer = new HighchartsRenderer(_chart);
+
+            _chart.XAxis = new List<Highsoft.Web.Mvc.Charts.XAxis>();
+            _chart.XAxis.Add(new Highsoft.Web.Mvc.Charts.XAxis());
+        }
+
+        [TestMethod]
+        public void TestBooleanPolarTrue()
+        {
+            
+
+            _chart.XAxis[0].AllowDecimals = false;            
+            string json = _renderer.RenderHtml();
+
+            Assert.IsTrue(json.Contains("\"allowDecimals\":false"));
+        }
+    }
+}
