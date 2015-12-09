0f3d1559a80da340fee11cfab56b27c9da0d3221
Rumen Stankov <rumen.stankov@gmail.com>
2015-12-09 11:07:56 +0200

    Fix the warnings in the sample customer project (the one customers get with the trial)

--- MVC_Demo/App_Start/BundleConfig.cs
+++ MVC_Demo/App_Start/BundleConfig.cs
@@ -17,7 +17,6 @@ namespace MVC_Demo
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
-        https://thepiratebay.se/
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
