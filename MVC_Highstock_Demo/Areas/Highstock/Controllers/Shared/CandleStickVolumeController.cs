5187929af8b109c2798ee952b7a3615928430acd
Rumen Stankov <rumen.stankov@gmail.com>
2016-01-20 18:08:58 +0200

    Setup and SQL Express database and port some of the data the comes from the REST APIs to the Database (to better show the server-side logic)

--- MVC_Highstock_Demo/Areas/Highstock/Controllers/Shared/CandleStickVolumeController.cs
+++ MVC_Highstock_Demo/Areas/Highstock/Controllers/Shared/CandleStickVolumeController.cs
@@ -1,7 +1,9 @@
 ﻿using Highsoft.Web.Mvc.Stocks;
+using MVC_Highstock_Demo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,38 @@ namespace MVC_Highstock_Demo.Areas.Highstock.Controllers.Shared
     {
         public ActionResult CandleStickVolume()
         {
+            string json;
+
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString("https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-ohlcv.json");
+            }
+
+            json = json.Substring(json.IndexOf('[') + 1);
+            json = json.Substring(json.IndexOf('[') + 1);
+
+            while (json.Length > 10)
+            {
+                string entity = json.Substring(0, json.IndexOf("]") - 1);
+
+                json = json.
+            }
+            
+
+
+            using (var db = new HighstockDataEntities())
+            {
+               //json.json = json
+
+                //db.CandlestickVolumes.Add(
+                //    new CandlestickVolume
+                //    {
+                //    }
+                //);
+            }
+
+            
+
             return View();
         }
 
