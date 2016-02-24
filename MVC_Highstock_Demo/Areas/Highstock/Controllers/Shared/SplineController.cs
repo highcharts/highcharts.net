7e96c6006c1ad4d5bc32d26358473cc39de7a910
Rumen Stankov <rumen.stankov@gmail.com>
2016-02-24 16:18:38 +0200

    Step Line Example

---
+++ MVC_Highstock_Demo/Areas/Highstock/Controllers/Shared/SplineController.cs
@@ -0,0 +1,36 @@
+﻿using Highsoft.Web.Mvc.Stocks;
+using MVC_Highstock_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Highstock_Demo.Areas.Highstock.Controllers.Shared
+{
+    public partial class SharedController : Controller
+    {
+        public ActionResult Spline()
+        {   
+            List<SplineSeriesData> appleData = new List<SplineSeriesData>();
+
+            using (var db = new HighstockDataEntities())
+            {
+                foreach (AppleData data in db.AppleDatas)
+                {
+                    appleData.Add(new SplineSeriesData
+                    {
+                        X = Convert.ToDouble(data.Date),
+                        Y = Convert.ToDouble(data.Value)
+                    });
+                }
+            }
+
+            ViewBag.AppleData = appleData.OrderBy(o => o.X).ToList();
+
+            return View(ViewBag);
+        }       
+
+    }
+}
