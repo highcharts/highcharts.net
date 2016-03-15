85513cb2b1c3ef3810699c01b002414ac7489f71
Rumen Stankov <rumen.stankov@gmail.com>
2016-03-15 11:49:27 +0200

    Fix 52,000 and 1.7 mil example to load data on client-side - server-side it too slow for the examples to be usable in all cases

--- MVC_Demo/Areas/Highstock/Controllers/Shared/LazyLoadingController.cs
+++ MVC_Demo/Areas/Highstock/Controllers/Shared/LazyLoadingController.cs
@@ -11,56 +11,10 @@ namespace MVC_Demo.Areas.Highstock.Controllers.Shared
 {
     public partial class SharedController : Controller
     {
-        List<double?> tempData = new List<double?>();
-
         public ActionResult LazyLoading()
         {            
-            List<LineSeriesData> temperatureData = new List<LineSeriesData>();  
-            
-            foreach (double? value in tempData)
-            {
-                temperatureData.Add(new LineSeriesData
-                {                   
-                    Y = value
-                });
-            }
-
-            ViewBag.TemperatureData = temperatureData.OrderBy(o => o.X).ToList();
-            return View(ViewBag);
+           
+            return View();
         }
-
-        private void LazyLoading_JsonToArray()
-        {
-            string url = "https://www.highcharts.com/samples/data/jsonp.php?filename=large-dataset.json";
-            string json;
-
-            using (WebClient wc = new WebClient())
-            {
-                json = wc.DownloadString(url);
-            }
-
-            json = json.Substring(json.IndexOf('[') + 1);
-
-            using (var db = new HighstockDataEntities())
-            {
-                while (true)
-                {
-                    double? value = null;
-                    if (json.IndexOf(',') == -1)
-                        break;
-
-                   string entity = json.Substring(0, json.IndexOf(','));
-
-                   if (entity != "null") 
-                       value = Convert.ToDouble(entity);
-
-                   data.Add(value);                      
-
-                    json = json.Substring(json.IndexOf(',') + 1);
-                }
-            }
-        }
-
-
     }
 }
