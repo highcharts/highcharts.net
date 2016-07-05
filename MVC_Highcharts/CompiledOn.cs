6ba9da339522d57ac78dfc9e16659bfc1c7e4ba8
Daniel Drwięga <daniel.drwiega@gmail.com>
2016-07-06 00:37:08 +0200

    changes in demo builder

--- MVC_Highcharts/CompiledOn.cs
+++ MVC_Highcharts/CompiledOn.cs
@@ -7,6 +7,6 @@ namespace Highsoft.Web.Mvc.Charts
 {
     internal static class CompiledOn
     {
-        internal static DateTime CompilationDate = new DateTime(2016, 5, 31);
+        internal static DateTime CompilationDate = new DateTime(2099, 1, 1);
     }
 }
