29b9063c139f5c4a7f2747a2a163d4cb49522bb3
Daniel Drwięga <brofixo@gmail.com>
2016-11-16 18:59:51 +0100

    API genenerator

---
+++ ApiGenerator/XmlDoc/doc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Xml.Serialization;
+
+namespace ApiGenerator.XmlDoc
+{
+    [XmlRoot("doc")]
+    public class Doc
+    {
+        [XmlElement("assembly")]
+        public Assembly Assembly {get;set;}
+
+        [XmlElement("members")]
+        public Members Members { get; set; }
+    }
+}
