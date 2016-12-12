d8792f2b84e84a2a99908ffb5576112f375fb3cb
Daniel Drwięga <brofixo@gmail.com>
2016-12-13 00:43:41 +0100

    website update - added titles and changed fonts

--- _HC5/MVC_Demo/Areas/Highcharts/Controllers/Demo/DemoController.cs
+++ _HC5/MVC_Demo/Areas/Highcharts/Controllers/Demo/DemoController.cs
@@ -85,6 +85,242 @@ namespace MVC_Demo.Areas.Highcharts.Controllers.Demo
             text = System.IO.File.ReadAllText(Server.MapPath("~/Areas/Highcharts/Views/Shared/" + demo + ".cshtml"));
             ViewBag.ViewCode = text;
 
+            switch(demo)
+            {
+                //line charts
+                case "LineBasic":
+                    ViewBag.Title = "Basic line";
+                    break;
+                case "LineAjax":
+                    ViewBag.Title = "Ajax loaded data, clickable points";
+                    break;
+                case "LineLabels":
+                    ViewBag.Title = "With data labels";
+                    break;
+                case "LineTimeSeries":
+                    ViewBag.Title = "Time series, zoomable";
+                    break;
+                case "SplineInverted":
+                    ViewBag.Title = "Spline with inverted axes";
+                    break;
+                case "SplineSymbols":
+                    ViewBag.Title = "Spline with symbols";
+                    break;
+                case "SplinePlotBands":
+                    ViewBag.Title = "Spline with plot bands";
+                    break;
+                case "SplineIrregularTime":
+                    ViewBag.Title = "Time data with irregular intervals";
+                    break;
+                case "LineLogAxis":
+                    ViewBag.Title = "Logarithmic axis";
+                    break;
+
+                //area charts
+                case "AreaBasic":
+                    ViewBag.Title = "Basic area";
+                    break;
+                case "AreaNegative":
+                    ViewBag.Title = "Area with negative values";
+                    break;
+                case "AreaStacked":
+                    ViewBag.Title = "Stacked area";
+                    break;
+                case "AreaStackedPercent":
+                    ViewBag.Title = "Percentage area";
+                    break;
+                case "AreaMissing":
+                    ViewBag.Title = "Area with missing points";
+                    break;
+                case "AreaInverted":
+                    ViewBag.Title = "Inverted axes";
+                    break;
+                case "Areaspline":
+                    ViewBag.Title = "Area-spline";
+                    break;
+                case "Arearange":
+                    ViewBag.Title = "Area range";
+                    break;
+                case "ArearangeLine":
+                    ViewBag.Title = "Area range and line";
+                    break;
+                case "Sparkline":
+                    ViewBag.Title = "Sparkline charts";
+                    break;
+
+                //column and bar charts
+                case "BarBasic":
+                    ViewBag.Title = "Basic bar";
+                    break;
+                case "BarStacked":
+                    ViewBag.Title = "Stacked bar";
+                    break;
+                case "BarNegativeStack":
+                    ViewBag.Title = "Bar with negative stack";
+                    break;
+                case "ColumnBasic":
+                    ViewBag.Title = "Basic column";
+                    break;
+                case "ColumnNegative":
+                    ViewBag.Title = "Column with negative values";
+                    break;
+                case "ColumnStacked":
+                    ViewBag.Title = "Stacked column";
+                    break;
+                case "ColumnStackedAndGrouped":
+                    ViewBag.Title = "Stacked and grouped column";
+                    break;
+                case "ColumnStackedPercent":
+                    ViewBag.Title = "Stacked percentage column";
+                    break;
+                case "ColumnRotatedLabels":
+                    ViewBag.Title = "Column with rotated labels";
+                    break;
+                case "ColumnDrilldown":
+                    ViewBag.Title = "Column with drilldown";
+                    break;
+                case "ColumnPlacement":
+                    ViewBag.Title = "Fixed placement columns";
+                    break;
+                case "ColumnParsed":
+                    ViewBag.Title = "Data defined in a HTML table";
+                    break;
+                case "Columnrange":
+                    ViewBag.Title = "Column range";
+                    break;
+
+                //Pie charts
+                case "PieBasic":
+                    ViewBag.Title = "Pie chart";
+                    break;
+                case "PieLegend":
+                    ViewBag.Title = "Pie with legend";
+                    break;
+                case "PieDonut":
+                    ViewBag.Title = "Donut chart";
+                    break;
+                case "PieSemiCircle":
+                    ViewBag.Title = "Semi circle donut";
+                    break;
+                case "PieDrilldown":
+                    ViewBag.Title = "Pie with drilldown";
+                    break;
+                case "PieGradient":
+                    ViewBag.Title = "Pie with gradient fill";
+                    break;
+                case "PieMonochrome":
+                    ViewBag.Title = "Pie with monochrome fill";
+                    break;
+
+                //Scatter and bubble charts
+                case "Scatter":
+                    ViewBag.Title = "Scatter plot";
+                    break;
+                case "Bubble":
+                    ViewBag.Title = "Bubble chart";
+                    break;
+
+                //Dynamic charts
+                case "DynamicUpdate":
+                    ViewBag.Title = "Spline updating each second";
+                    break;
+                case "DynamicClickToAdd":
+                    ViewBag.Title = "Click to add a point";
+                    break;
+                case "DynamicMasterDetail":
+                    ViewBag.Title = "Master-detail chart";
+                    break;
+
+                //Combinations
+                case "Combo":
+                    ViewBag.Title = "Column, line and pie";
+                    break;
+                case "ComboDualAxes":
+                    ViewBag.Title = "Dual axes, line and column";
+                    break;
+                case "ComboMultiAxes":
+                    ViewBag.Title = "Multiple axes";
+                    break;
+                
+                case "ComboRegression":
+                    ViewBag.Title = "Scatter with regression line";
+                    break;
+                case "ComboMeteogram":
+                    ViewBag.Title = "Meteogram";
+                    break;
+                case "ComboTimeline":
+                    ViewBag.Title = "Advanced timeline";
+                    break;
+
+                //3d charts
+                case "Column3DInteractive":
+                    ViewBag.Title = "3D column";
+                    break;
+                case "Column3DNullValues":
+                    ViewBag.Title = "3D column with null and 0 values";
+                    break;
+                case "Column3DStackingGrouping":
+                    ViewBag.Title = "3D column with stacking and grouping";
+                    break;
+                case "Pie3D":
+                    ViewBag.Title = "3D pie";
+                    break;
+                case "Pie3DDonut":
+                    ViewBag.Title = "3D donut";
+                    break;
+
+
+                //Gauges
+                case "Heatmap":
+                    ViewBag.Title = "Heat map";
+                    break;
+                case "HeatmapCanvas":
+                    ViewBag.Title = "Large heat map";
+                    break;
+                case "TreemapColoraxis":
+                    ViewBag.Title = "Tree map with color axis";
+                    break;
+                case "TreemapWithLevels":
+                    ViewBag.Title = "Tree map with levels";
+                    break;
+                case "TreemapLargeDataset":
+                    ViewBag.Title = "Large tree map";
+                    break;
+
+                case "Polar":
+                    ViewBag.Title = "Polar chart";
+                    break;
+                case "PolarSpider":
+                    ViewBag.Title = "Spiderweb";
+                    break;
+                case "PolarWindRose":
+                    ViewBag.Title = "Wind rose";
+                    break;
+                case "BoxPlot":
+                    ViewBag.Title = "Box plot";
+                    break;
+                case "ErrorBar":
+                    ViewBag.Title = "Error bar";
+                    break;
+                case "Waterfall":
+                    ViewBag.Title = "Waterfall";
+                    break;
+                case "Funnel":
+                    ViewBag.Title = "Funnel chart";
+                    break;
+                case "Pyramid":
+                    ViewBag.Title = "Pyramid chart";
+                    break;
+                case "Polygon":
+                    ViewBag.Title = "Polygon series";
+                    break;
+                case "Renderer":
+                    ViewBag.Title = "General drawing";
+                    break;
+            }
+
+            ViewBag.Title += " | Highcharts.NET";
+
             return View();
         }
 
--- _HC5/MVC_Demo/Areas/Highstock/Controllers/Demo/DemoController.cs
+++ _HC5/MVC_Demo/Areas/Highstock/Controllers/Demo/DemoController.cs
@@ -77,7 +77,104 @@ namespace MVC_Demo.Areas.Highstock.Controllers.Demo
             text = System.IO.File.ReadAllText(Server.MapPath("~/Areas/Highstock/Views/Shared/" + demo + ".cshtml"));
             ViewBag.ViewCode = text;
 
-            return View();
+            switch (demo)
+            {
+                //line charts
+                case "BasicLine":
+                    ViewBag.Title = "Single line series";
+                    break;
+                case "CandlestickAndVolume":
+                    ViewBag.Title = "Two panes, candlestick and volume";
+                    break;
+                case "Compare":
+                    ViewBag.Title = "Compare multiple series";
+                    break;
+                case "DataGrouping":
+                    ViewBag.Title = "52,000 points with data grouping";
+                    break;
+                case "LazyLoading":
+                    ViewBag.Title = "1,7 million points with async loading";
+                    break;
+                case "IntradayArea":
+                    ViewBag.Title = "Intraday Area";
+                    break;
+                case "IntradayBreaks":
+                    ViewBag.Title = "Intraday with breaks";
+                    break;
+                case "IntradayCandlestick":
+                    ViewBag.Title = "Intraday Candlestick";
+                    break;
+                case "FlagsGeneral":
+                    ViewBag.Title = "Flags marking events";
+                    break;
+                case "DynamicUpdate":
+                    ViewBag.Title = "Dynamically Updated Data";
+                    break;
+                case "LineMarkers":
+                    ViewBag.Title = "Line with markers and shadow";
+                    break;
+                case "Spline":
+                    ViewBag.Title = "Spline";
+                    break;
+                case "StepLine":
+                    ViewBag.Title = "Step line";
+                    break;
+                case "Area":
+                    ViewBag.Title = "Area";
+                    break;
+                case "Areaspline":
+                    ViewBag.Title = "Area spline";
+                    break;
+                case "Arearange":
+                    ViewBag.Title = "Area range";
+                    break;
+                case "Areasplinerange":
+                    ViewBag.Title = "Area spline range";
+                    break;
+                case "Candlestick":
+                    ViewBag.Title = "Candlestick";
+                    break;
+                case "Ohlc":
+                    ViewBag.Title = "OHLC";
+                    break;
+                case "Column":
+                    ViewBag.Title = "Column";
+                    break;
+                case "Columnrange":
+                    ViewBag.Title = "Column range";
+                    break;
+                case "MarkersOnly":
+                    ViewBag.Title = "Point markers only";
+                    break;
+                case "YaxisPlotlines":
+                    ViewBag.Title = "Plot lines on Y axis";
+                    break;
+                case "YaxisPlotbands":
+                    ViewBag.Title = "Plot bands on Y axis";
+                    break;
+                case "YaxisReversed":
+                    ViewBag.Title = "Reversed Y axis";
+                    break;
+                case "StyledScrollbar":
+                    ViewBag.Title = "Styled scrollbar";
+                    break;
+                case "ScrollbarDisabled":
+                    ViewBag.Title = "Disabled scrollbar";
+                    break;
+                case "NavigatorDisabled":
+                    ViewBag.Title = "Disabled navigator";
+                    break;
+                case "FlagsPlacement":
+                    ViewBag.Title = "Flags placement";
+                    break;
+                case "FlagsShapes":
+                    ViewBag.Title = "Flags shapes and colors";
+                    break;
+            }
+
+            ViewBag.Title += " | Highstock.NET";
+
+                    return View();
         }
 
 
