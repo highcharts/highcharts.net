14b5ff61cd1b1ca1cfa2cb1ef34d5c4f39eaf6df
Daniel Drwięga <brofixo@gmail.com>
2017-10-16 19:08:54 +0200

    Add key generator and update key for website for version 6

---
+++ KeyGen/LicenseKeyGenerator/LicenseKeyGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseKeyGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "Ver6NetWrap8686308";
+            byte[] bytes = Encoding.UTF8.GetBytes(key);
+            string base64 = Convert.ToBase64String(bytes);
+
+            Console.WriteLine(base64);
+            Console.ReadLine();
+        }
+    }
+}
