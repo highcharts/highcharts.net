63aad9baf88316e8ee3217f917b5b4a19921a4e6
Daniel Drwiega <brofixo@gmail.com>
2018-12-17 14:56:56 +0100

    Update MVC Highstock

---
+++ _HC6/MVC_Highstock/CodeGeneration/NavigatorXAxisCrosshairLabelStyle.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.Collections;
+using System;
+using System.Collections.Specialized;
+using System.Web;
+using System.IO;
+
+namespace Highsoft.Web.Mvc.Stocks
+{
+	public partial class NavigatorXAxisCrosshairLabelStyle  : BaseObject
+	{
+		public NavigatorXAxisCrosshairLabelStyle()
+		{
+			Color = Color_DefaultValue = "white";
+			FontSize = FontSize_DefaultValue = "11px";
+			FontWeight = FontWeight_DefaultValue = "normal";
+			TextAlign = TextAlign_DefaultValue = "center";
+			
+		}	
+		
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Color { get; set; }
+		private string Color_DefaultValue { get; set; }
+		 
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string FontSize { get; set; }
+		private string FontSize_DefaultValue { get; set; }
+		 
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string FontWeight { get; set; }
+		private string FontWeight_DefaultValue { get; set; }
+		 
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string TextAlign { get; set; }
+		private string TextAlign_DefaultValue { get; set; }
+		  
+
+		internal override Hashtable ToHashtable()
+		{
+			Hashtable h = new Hashtable();
+
+			if (Color != Color_DefaultValue) h.Add("color",Color);
+			if (FontSize != FontSize_DefaultValue) h.Add("fontSize",FontSize);
+			if (FontWeight != FontWeight_DefaultValue) h.Add("fontWeight",FontWeight);
+			if (TextAlign != TextAlign_DefaultValue) h.Add("textAlign",TextAlign);
+			
+
+			return h;
+		}
+
+		internal override string ToJSON()
+		{            
+			Hashtable h = ToHashtable();
+			JavaScriptSerializer serializer = new JavaScriptSerializer();
+            serializer.MaxJsonLength = Int32.MaxValue;
+
+			if (h.Count > 0)
+				return serializer.Serialize(ToHashtable());
+			else 
+				return "";
+		}       
+
+		// checks if the state of the object is different from the default
+		// and therefore needs to be serialized
+		internal override bool IsDirty()
+		{
+			return ToHashtable().Count > 0;
+		}
+	}
+}
