7f7d1f575ecc3248760fb9f7188337e37aa402c3
Daniel Drwiega <brofixo@gmail.com>
2019-10-31 08:18:04 +0100

    Update generating Highstock logic

---
+++ _HC7/SourceCodeGenerator/Services/ApiItemComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SourceCodeGenerator.Services
+{
+    class ApiItemComparer : IEqualityComparer<ApiItem>
+    {
+        public bool Equals(ApiItem item1, ApiItem item2)
+        {
+            return item1.Title.Equals(item2.Title);
+        }
+
+        public int GetHashCode(ApiItem item)
+        {
+            return item.GetHashCode();
+        }
+    }
+}
