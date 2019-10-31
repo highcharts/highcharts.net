14be86c9af82102ba0eeba9868e94178590c99aa
Daniel Drwiega <brofixo@gmail.com>
2019-10-31 08:11:56 +0100

    Update SetOptions sample project

---
+++ Demos/WebForms/HighchartsWebForms/HighchartsWebForms/Controls/SetOptionsControl.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="SetOptionsControl.ascx.cs" Inherits="HighchartsWebForms.Controls.SetOptionsControl" %>
---
+++ Demos/WebForms/HighchartsWebForms/HighchartsWebForms/Controls/SetOptionsControl.ascx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Highsoft.Web.Mvc.Charts;
+using Highsoft.Web.Mvc.Charts.Rendering;
+
+namespace HighchartsWebForms.Controls
+{
+    public partial class SetOptionsControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var renderer = new HighchartsRenderer();
+
+            Response.Write("<div id='aaa'></div>"+renderer.SetOptions(null, new Lang
+            {
+                Months = new List<string> { "styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień" },
+                ShortMonths = new List<string> { "sty", "lut", "mar", "kwi", "maj", "cze", "lip", "sie", "wrz", "paź", "lis", "gru" }
+            }));
+        }
+    }
+}
