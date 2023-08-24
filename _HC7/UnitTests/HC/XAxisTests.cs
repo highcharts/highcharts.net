249ea63fb6d11c7b18377b8ea6164b2298bed65e
Daniel Drwiega <brofixo@gmail.com>
2023-08-24 21:51:17 +0200

    Add XAxis.Breaks test

---
+++ _HC7/UnitTests/HC/XAxisTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Highsoft.Web.Mvc.Charts;
+using Highsoft.Web.Mvc.Charts.Rendering;
+using System.Collections;
+using System.Globalization;
+using TH = Tests.Helpers.TestHelper;
+using UnitTests.HC;
+
+namespace UnitTests_HC.HC
+{
+    public class XAxisTests : IClassFixture<HcFixture>
+    {
+        protected HcFixture _fixture;
+
+        public XAxisTests(HcFixture fixture)
+        {
+            _fixture = fixture;
+            _fixture.ChartType = ChartType.Line;
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3, 4)]
+        [InlineData(5, 2, 30, 5)]
+        public void Test_IfXAxisBreaksRenders_Correct(double breakSize, double from, double to, double repeat)
+        {
+            var chart = new Highcharts();
+            var renderer = new HighchartsRenderer(chart);
+            
+            chart.XAxis = new List<XAxis> { new XAxis { Breaks = new List<XAxisBreaks> { new XAxisBreaks() { BreakSize = breakSize, From = from, To = to, Repeat = repeat } } } };
+
+            var result = renderer.RenderHtml();
+
+            //Test should be updated...
+            Assert.Contains("breaks", result);
+        }
+    }
+}
