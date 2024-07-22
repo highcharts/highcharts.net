04b4dc0a8fc00991730674087e94f5f51a9439d8
Daniel Drwiega <brofixo@gmail.com>
2024-07-22 09:34:17 +0200

    Remove CompiledOn files

--- _HC7/HighchartsStandard/CompiledOn.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Highsoft.Web.Mvc.Charts
-{
-    internal static class CompiledOn
-    {
-        internal static DateTime CompilationDate = new DateTime(2019, 5, 22);
-    }
-}
